d7e6961b24b4d5ef09d16aeb6f126a513a7e8d04
anujdubeyop <74654451+anujdubeyop@users.noreply.github.com>
2021-03-30 06:44:04 +0530

    Update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { AuthGuardService } from './services/auth-guard.service';
+
+const routes: Routes = [{
+  path: '',
+  children: []
+},
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'auth-callback',
+    component: AuthCallbackComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
